c6ab97053e84adef5c19ce84ee246b700813108a
Florian Gudat <guddii@users.noreply.github.com>
2017-09-19 20:39:18 +0200

    Add splash page (#1) (#2)
    * Add splash page

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
+app.use(express.static(__dirname + '/static'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+  response.render('pages/index', {
+    pkg: require('./package.json'),
+    app: require('./app.json'),
+    content: require('./views/data/content.json')
+  });
+});
+app.get("/profile/:profile", function (request, response) {
+  response.render('pages/profile', {
+    pkg: require('./package.json'),
+    app: require('./app.json'),
+    content: require('./views/data/content.json'),
+    request: request.params
+  });
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
