be32a9f0edc3e63a6bbe5894ac8e6323cd940fe0
Florian Gudat <guddii@users.noreply.github.com>
2017-09-27 20:09:54 +0200

    Feature cards (#14)
    * Add breadcrumbs
    
    * Add breadcrumbs
    
    * Update sitemap
    
    * Update microdata

--- index.js
+++ index.js
@@ -12,7 +12,19 @@ app.get('/', function (request, response) {
   response.render('pages/index', {
     pkg: require('./package.json'),
     app: require('./app.json'),
-    content: require('./views/data/content.json')
+    content: require('./views/data/content.json'),
+    settings: {
+      home: true
+    }
+  });
+});
+
+app.get("/profiles", function (request, response) {
+  response.render('pages/profiles', {
+    pkg: require('./package.json'),
+    app: require('./app.json'),
+    content: require('./views/data/content.json'),
+    request: request.params
   });
 });
 
@@ -25,8 +37,8 @@ app.get("/profile/:profile", function (request, response) {
   });
 });
 
-app.use(express.static(__dirname + '/static', { maxAge: 31557600000 }));
-app.use(function(req, res) {
+app.use(express.static(__dirname + '/static', {maxAge: 31557600000}));
+app.use(function (req, res) {
   res.status(404).render('error/404', {
     pkg: require('./package.json'),
     app: require('./app.json'),
